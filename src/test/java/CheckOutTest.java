5fefaf479c3d81459cd2c607d90474994dfa38ea
Alma Nursultan <alma@Almas-Air.lan>
2024-03-06 20:04:10 -0700

    first commit

---
+++ src/test/java/CheckOutTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class CheckOutTest {
+    @Test
+    public void oneTest(){
+        System.out.println(" the number one");
+    }
+    @Test
+    public void twoTest(){
+        System.out.println("the number two");
+    }
+}
---
+++ src/test/java/LoginTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+public class LoginTest {
+    @Test
+    public void testLogin(){
+        System.out.println(" is my first test");
+    }
+    @Test
+    public void testLogin2(){
+        System.out.println("this is the second one");
+    }
+}
